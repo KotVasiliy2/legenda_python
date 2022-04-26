d82a1af5ed4129bd4b964712f3a740abf8026228
legendaOS <76245715+legendaOS@users.noreply.github.com>
2022-04-26 19:50:33 +0300

    Create main.py

---
+++ Задания/#ВЕРНИТЕЛИПТОН/#НЕЧЕСНОВЕРНИТЕЛИПТОН/rumeninteherc/main.py
@@ -0,0 +1,159 @@
+a=None
+while a!=0:
+    a=int(input("введите цивру"))
+    if a==1:
+        print("I")
+    if a==2:
+        print("II")
+    if a==3:
+        print("III")
+    if a==4:
+        print("IV")
+    if a==5:
+        print("V")
+    if a==6:
+        print("VI")
+    if a==7:
+        print("VII")
+    if a==8:
+        print("VIII")
+    if a==9:
+        print("IX")
+    if a==10:
+        print("X")
+    if a==11:
+        print("XI")
+    if a==12:
+        print("XII")
+    if a==13:
+        print("XIII")
+    if a==14:
+        print("XIV")
+    if a==15:
+        print("XV")
+    if a==16:
+        print("XVI")
+    if a==17:
+        print("XVII")
+    if a==18:
+        print("XVIII")
+    if a==19:
+        print("XIX")
+    if a==20:
+        print("XX")
+    if a==21:
+        print("XXI")
+    if a==22:
+        print("XXII")
+    if a==23:
+        print("XXIII")
+    if a==24:
+        print("XXIV")
+    if a==25:
+        print("XXV")
+    if a==26:
+        print("XXVI")
+    if a==27:
+        print("XXVII")
+    if a==28:
+        print("XXVIII")
+    if a==29:
+        print("XXIX")
+    if a==30:
+        print("XXX")
+    if a==31:
+        print("XXXI")
+    if a==32:
+        print("XXXII")
+    if a==33:
+        print("XXXIII")
+    if a==34:
+        print("XXXIV")
+    if a==35:
+        print("XXXV")
+    if a==36:
+        print("XXXVI")
+    if a==37:
+        print("XXXVII")
+    if a==38:
+        print("XXXVIII")
+    if a==39:
+        print("XXXIX")
+    if a==40:
+        print("XXXX")
+    if a==41:
+        print("XXXXI")
+    if a==42:
+        print("XXXXII")
+    if a==43:
+        print("XXXXIII")
+    if a==44:
+        print("XXXXIV")
+    if a==45:
+        print("XXXXV")
+    if a==777:
+        print("ИкЕс бОкЕс или БаБеДжИ")
+    if a==46:
+        print("XXXXVI")
+    if a==47:
+        print("XXXXVII")
+    if a==48:
+        print("XXXXVIII")
+    if a==49:
+        print("XXXXIX")
+    if a==50:
+        print("XXXXX")
+    if a==51:
+        print("XXXXXI")
+    if a==52:
+        print("XXXXXII")
+    if a==53:
+        print("XXXXXIII")
+    if a==54:
+        print("XXXXXIV")
+    if a==55:
+        print("XXXXXV")
+    if a==56:
+        print("XXXXXVI")
+    if a==57:
+        print("XXXXXVII")
+    if a==58:
+        print("XXXXXVIII")
+    if a==59:
+        print("XXXXXIX")
+    if a==60:
+        print("XXXXXX")
+    if a==61:
+        print("XXXXXXI")
+    if a==62:
+        print("XXXXXXII")
+    if a==63:
+        print("XXXXXXIII")
+    if a==64:
+        print("XXXXXXIV")
+    if a==65:
+        print("XXXXXXV")
+    if a==66:
+        print("XXXXXXVI")
+    if a==67:
+        print("XXXXXXVII")
+    if a==68:
+        print("XXXXXXVIII")
+    if a==69:
+        print("XXXXXXIX")
+    if a==70:
+        print("XXXXXXX")
+    if a==71:
+        print("XXXXXXXI")
+    if a==72:
+        print("XXXXXXXII")
+    if a==73:
+        print("XXXXXXXIII")
+    if a==74:
+        print("XXXXXXXIV")
+    if a==75:
+        print("XXXXXXXV")
+    if a==76:
+        print("XXXXXXXVI")
+    if a>=77:
+        print("СлИшКоМ БоЛьШоЕ ЧиСлО ЗдОб АшИбКа НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь")
