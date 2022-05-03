94756dcdf4fa7f84f0e3e1381db37f40b48f1d51
KotVasiliy228 <97304366+KotVasiliy228@users.noreply.github.com>
2022-05-03 18:34:20 +0300

    zessxrdctfvygbuhnijmok,p.;/'

---
+++ Задания/#ВЕРНИТЕЛИПТОН/#НЕЧЕСНОВЕРНИТЕЛИПТОН/rumeninteherc/main.py
@@ -0,0 +1,159 @@
+a=None
+while a!=0:
+    a=int(input("введите цивру"))
+    if a==1:
+        print("I")
+    elif a==2:
+        print("II")
+    elif a==3:
+        print("III")
+    elif a==4:
+        print("IV")
+    elif a==5:
+        print("V")
+    elif a==6:
+        print("VI")
+    elif a==7:
+        print("VII")
+    elif a==8:
+        print("VIII")
+    elif a==9:
+        print("IX")
+    elif a==10:
+        print("X")
+    elif a==11:
+        print("XI")
+    elif a==12:
+        print("XII")
+    elif a==13:
+        print("XIII")
+    elif a==14:
+        print("XIV")
+    elif a==15:
+        print("XV")
+    elif a==16:
+        print("XVI")
+    elif a==17:
+        print("XVII")
+    elif a==18:
+        print("XVIII")
+    elif a==19:
+        print("XIX")
+    elif a==20:
+        print("XX")
+    elif a==21:
+        print("XXI")
+    elif a==22:
+        print("XXII")
+    elif a==23:
+        print("XXIII")
+    elif a==24:
+        print("XXIV")
+    elif a==25:
+        print("XXV")
+    elif a==26:
+        print("XXVI")
+    elif a==27:
+        print("XXVII")
+    elif a==28:
+        print("XXVIII")
+    elif a==29:
+        print("XXIX")
+    elif a==30:
+        print("XXX")
+    elif a==31:
+        print("XXXI")
+    elif a==32:
+        print("XXXII")
+    elif a==33:
+        print("XXXIII")
+    elif a==34:
+        print("XXXIV")
+    elif a==35:
+        print("XXXV")
+    elif a==36:
+        print("XXXVI")
+    elif a==37:
+        print("XXXVII")
+    elif a==38:
+        print("XXXVIII")
+    elif a==39:
+        print("XXXIX")
+    elif a==40:
+        print("XXXX")
+    elif a==41:
+        print("XXXXI")
+    elif a==42:
+        print("XXXXII")
+    elif a==43:
+        print("XXXXIII")
+    elif a==44:
+        print("XXXXIV")
+    elif a==45:
+        print("XXXXV")
+    elif a==777:
+        print("ИкЕс бОкЕс или БаБеДжИ")
+    elif a==46:
+        print("XXXXVI")
+    elif a==47:
+        print("XXXXVII")
+    elif a==48:
+        print("XXXXVIII")
+    elif a==49:
+        print("XXXXIX")
+    elif a==50:
+        print("XXXXX")
+    elif a==51:
+        print("XXXXXI")
+    elif a==52:
+        print("XXXXXII")
+    elif a==53:
+        print("XXXXXIII")
+    elif a==54:
+        print("XXXXXIV")
+    elif a==55:
+        print("XXXXXV")
+    elif a==56:
+        print("XXXXXVI")
+    elif a==57:
+        print("XXXXXVII")
+    elif a==58:
+        print("XXXXXVIII")
+    elif a==59:
+        print("XXXXXIX")
+    elif a==60:
+        print("XXXXXX")
+    elif a==61:
+        print("XXXXXXI")
+    elif a==62:
+        print("XXXXXXII")
+    elif a==63:
+        print("XXXXXXIII")
+    elif a==64:
+        print("XXXXXXIV")
+    elif a==65:
+        print("XXXXXXV")
+    elif a==66:
+        print("XXXXXXVI")
+    elif a==67:
+        print("XXXXXXVII")
+    elif a==68:
+        print("XXXXXXVIII")
+    elif a==69:
+        print("XXXXXXIX")
+    elif a==70:
+        print("XXXXXXX")
+    elif a==71:        
+        print("XXXXXXXI")
+    elif a==72:
+        print("XXXXXXXII")
+    elif a==73:
+        print("XXXXXXXIII")
+    elif a==74:
+        print("XXXXXXXIV")
+    elif a==75:
+        print("XXXXXXXV")
+    elif a==76:
+        print("XXXXXXXVI")
+    elif a>=77:
+        print("СлИшКоМ БоЛьШоЕ ЧиСлО ЗдОб АшИбКа НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь НуЛь")
